d6dffa803cc98cd1f4007725c78ba9dab4dc4b99
alu0100769579 <alu0100769579@ull.edu.es>
2015-10-29 14:00:06 +0000

    Arreglado mcm y añadido mcd, para que haga bien la resta y la suma

--- lib/numFraccionarios.rb
+++ lib/numFraccionarios.rb
@@ -1,13 +1,20 @@
 class NumFraccionarios
   
   attr_reader :numerador, :denominador
+  
+  
+  
   def initialize(numerador, denominador)
     @numerador, @denominador = numerador, denominador
   end
   
+  
   def to_s
     "(#{@numerador},#{@denominador})"
   end
+  
+  
+  
   
   
   def * (value)
@@ -19,11 +26,13 @@ class NumFraccionarios
   end
   
   def + (other)
-      NumFraccionarios.new(((@numerador * other.denominador) + (other.numerador * @denominador)), @denominador * other.denominador)
+      aux = mcm(@denominador, other.denominador)
+      NumFraccionarios.new(((aux/@denominador)*@numerador) + ((aux/other.denominador) * other.numerador), aux)
   end
   
   def - (other)
-      NumFraccionarios.new(((@numerador * other.denominador) - (other.numerador * @denominador)), @denominador * other.denominador)
+      aux = mcm(@denominador, other.denominador)
+	    NumFraccionarios.new(((aux/@denominador)*@numerador)-((aux/other.denominador)*other.numerador), aux)
   end
   
   def ** (other)
@@ -33,5 +42,21 @@ class NumFraccionarios
   def / (other)
       NumFraccionarios.new(@numerador * other.denominador, @denominador * other.numerador)
   end
+  
+  def mcd(num, den)
+  	#Maximo comun divisor
+  	if num == 0
+		  den
+	  else
+		  mcd(den%num, num)
+	end
+  end
+  
+  def mcm(den1, den2)
+  	#Minimo comun multiplo
+  	(den1/mcd(den1,den2))*den2
+  end  
+  
+  
   
 end
--- test/testNumFraccionarios.rb
+++ test/testNumFraccionarios.rb
@@ -4,20 +4,44 @@ require 'test/unit'
 class TestNumNumFraccionariosarios < Test::Unit::TestCase
   
   def setup
-    @origen = NumFraccionarios.new(2,7)
-    @unidad = NumFraccionarios.new(3,4)
+    @origen = NumFraccionarios.new(2,6)
+    @unidad = NumFraccionarios.new(3,9)
     
   end
   
-  def test_simple
-    assert_equal("(2,7)", @origen.to_s)
-    assert_equal("(15,20)", (@unidad*5).to_s)
-    assert_equal("(-3,-4)", (-@unidad).to_s)
-    assert_equal("(29,28)", (@origen + @unidad).to_s)
-    assert_equal("(-13,28)", (@origen - @unidad).to_s)
-    assert_equal("(6,28)", (@origen ** @unidad).to_s)
-    assert_equal("(8,21)", (@origen / @unidad).to_s)
-
+  def test_equal
+    assert_equal("(2,6)", @origen.to_s)
+    
   end
+  
+  def test_mul_esc
+    assert_equal("(15,45)", (@unidad*5).to_s)
+  
+  end
+  
+  def test_neg
+    assert_equal("(-3,-9)", (-@unidad).to_s)
+  
+  end
+  
+  def test_sum
+    assert_equal("(12,18)", (@origen + @unidad).to_s)
+    
+  end
+  
+  def test_rest
+    assert_equal("(0,18)", (@origen - @unidad).to_s)
+  
+  end
+  
+  def test_mult
+    assert_equal("(6,54)", (@origen ** @unidad).to_s)
+    
+  end
+  
+  def test_div
+    assert_equal("(18,18)", (@origen / @unidad).to_s)
+  end
+    
 
 end
